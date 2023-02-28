f15e6a7d67428dde94144e7666f4ca3763d1989c
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-28 13:55:51 +0300

    first commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,27 @@
 package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        for (int item: sourceList) {
+            if(item%2==0){
+                while (item%2==0){
+                    hashSet.add(item);
+                    item=item/2;
+                }
+                hashSet.add(item);
+            }else{
+                int x=item;
+                while (item<=(2*x)){
+                    hashSet.add(item);
+                    item=2*item;
+                }
+            }
+        }
+        return hashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,16 @@
 package com.epam.mjc.collections.set;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> treeSet=new TreeSet<>();
+        Iterator<Integer> iterator=sourceList.iterator();
+        while (iterator.hasNext()){
+            int element=iterator.next();
+            treeSet.add((int)Math.pow(element,2));
+        }
+        Set<Integer> set=new LinkedHashSet<>(treeSet.subSet(lowerBound,upperBound+1));
+        return set;
     }
 }
